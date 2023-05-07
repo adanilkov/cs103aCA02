3559ca5b5dc11b804fe7bcf51c47c250a53568fb
Alex <62477923+adanilkov@users.noreply.github.com>
2023-05-07 00:09:46 -0400

    finished refactor route

--- firstapp/routes/refactor.js
+++ firstapp/routes/refactor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const { isLoggedIn } = require('./pwauth');
 
 isLoggedIn = (_,res,next) => {
     if (res.locals.loggedIn) {
@@ -14,6 +15,15 @@ router.get('/refactor', isLoggedIn, async (req, res) => {
     res.render('refactor')
 })
 
-// router.post()
+router.get('/refactor/gpt', isLoggedIn, async (req, res) => {
+    if (req.body.prompt != null) {
+        res.status(204).json({error: 'no prompt'}).send();
+    } else {
+        const resp = await axios.post('http://gracehopper.cs-i.brandeis.edu:3500/openai',
+            {prompt:req.body.prompt})
+
+        res.status(200).send(resp.data.choices[0]['message']['content']);
+    }
+})
 
 module.exports = router;
